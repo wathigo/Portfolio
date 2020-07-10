4f7d8576fa825ea078ad8bd6be8fc8c0262ac4ce
wathigo <wathigosimon@gmail.com>
2020-07-10 17:50:37 +0300

    Update Skill component

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,59 +1,65 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-import ReactWOW from 'react-wow';
+import { Spring } from "react-spring/renderprops";
+import VisibilitySensor from "./VisibilitySensor";
 
-class Skill extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  constructor(props) {
-    super(props);
-    this.showOverlay = this.showOverlay.bind(this);
-    this.hideOverlay = this.hideOverlay.bind(this);
-  }
+const Skill = (props) => {
+  const rootNode = useRef(null);
 
-  showOverlay(id) { // eslint-disable-line
+  const showOverlay = (id) => { // eslint-disable-line
     document.querySelector(`.${id}`).style.visibility = 'visible';
   }
 
-  hideOverlay(id) { // eslint-disable-line
+  const hideOverlay = (id) => { // eslint-disable-line
     document.querySelector(`.${id}`).style.visibility = 'hidden';
   }
 
-  render() {
-    const { data } = this.props;
-    const description = data.description.map(desc => <li>{desc}</li>); // eslint-disable-line
-    const technologies = data.tec.map(desc => <li>{desc}</li>); // eslint-disable-line
-    return (/* eslint-disable */
-      <ReactWOW animation={data.animation} scroll data-wow-duration="2s" data-wow-delay="5s">
-        <div className="skill wow">
-          <div onMouseEnter={() => { this.showOverlay(`o-${data.id}`); }} onMouseLeave={() => { this.hideOverlay(`o-${data.id}`); }} className="img-cont">
-            <div className={`overlay + o-${data.id}`}>
-              <a href={data.src} className='l-1' rel="noopener noreferrer" target="_blank"><i className="fas fa-link"></i> Live Demo </a>
-              <a href={data.github} className='l-2' rel="noopener noreferrer" target="_blank"><i className="fab fa-github-alt"></i> Git Repo </a>
-            </div>
-            <figure>
-              <img src={require(`../images/${data.id}.png`)} alt="project screenshot" />
-            </figure>
-          </div>
-          <div className="desc">
-            <h3>{data.name}</h3>
-            <div className='head'><h4>Knowledge Acquired</h4></div>
-            <ul>
-              {
-              description
-          }
-            </ul>
-          </div>
+  const { data } = props;
+  const technologies = data.tec.map(desc => <li>{desc}</li>); // eslint-disable-line
 
-          <div className="tec-cont">
-            <h4>Technologies</h4>
-            <ul>
-              {technologies}
-            </ul>
-          </div>
+  return ( /* eslint-disable */
+    <div>
+      <VisibilitySensor once partialVisibility>
+        {({ isVisible }) => (
+          <Spring delay={300} to={{ opacity: isVisible ? 1 : 0 }}>
+            {({ opacity }) => (
+              <div style={{ opacity }} ref={rootNode} className="skill wow">
+                <div onMouseEnter={() => { showOverlay(`o-${data.id}`); }} onMouseLeave={() => { hideOverlay(`o-${data.id}`); }} className="img-cont">
+                  <div className={`overlay + o-${data.id}`}>
+                    <p>
+                      <a href={data.src} className='l-1' rel="noopener noreferrer" target="_blank"><i className="fas fa-link"></i> Live Demo </a>
+                      <a href={data.github} className='l-2' rel="noopener noreferrer" target="_blank"><i className="fab fa-github-alt"></i> Git Repo </a>
+                    </p>
+                  </div>
+                  <figure>
+                    <img src={require(`../images/${data.id}.png`)} alt="project screenshot" />
+                  </figure>
+                </div>
+                <div className='btm'>
+                  <div className="desc">
+                    <h3>{data.name}</h3>
+                    <div className='head'><h4>About Project</h4></div>
+                    <p>
+                      {data.description}
+                    </p>
+                  </div>
 
-        </div>
-      </ReactWOW>
-    );
-  }
+                  <div className="tec-cont">
+                    <h4>Technologies</h4>
+                    <ul>
+                      {technologies}
+                    </ul>
+                  </div>
+                </div>
+
+              </div>
+            )}
+          </Spring>
+        )}
+      </VisibilitySensor>
+
+    </div >
+  );
 }
 
 Skill.propTypes = {
