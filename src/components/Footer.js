b5db6a772d5ca2906c730620a96654552a0208d4
wathigo <wathigosimon@gmail.com>
2020-07-13 07:52:14 +0300

    Update footer with profile links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,18 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      Project by Simon Wathigo @2019
+      <p>Project by Simon Wathigo @2019</p>
+      <ul>
+        <li>
+          <a class="icons-footer" href="https://linkedin.com/in/simon-wathigo-445370183"><i class="fab fa-linkedin-in"></i></a>
+        </li>
+        <li>
+          <a class="icons-footer" href="https://github.com/wathigo"><i class="fab fa-github"></i></a>
+        </li>
+        <li>
+          <a class="icons-footer" href="https://twitter.com/simon_wathigo"><i class="fab fa-twitter"></i></a>
+        </li>
+      </ul>
     </footer>
   );
 }
